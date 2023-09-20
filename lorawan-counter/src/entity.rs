1286b19ef5b678e81e496153867414dfd402de38
vxshug <vxzyfx@gmail.com>
2023-09-20 18:58:11 +0800

    feat: stream

--- lorawan-counter/src/entity.rs
+++ lorawan-counter/src/entity.rs
@@ -13,3 +13,31 @@ pub struct Applictaion {
 pub struct ApplictaionID {
     pub application_id: String
 }
+
+#[derive(Debug, serde::Serialize, serde::Deserialize)]
+pub struct Identifiers {
+    pub identifiers: Vec<Identifier>,
+    pub names: Vec<String>
+}
+
+#[derive(Debug, serde::Serialize, serde::Deserialize)]
+pub struct Identifier {
+    pub device_ids: DeviceId
+}
+
+#[derive(Debug, serde::Serialize, serde::Deserialize)]
+pub struct DeviceId {
+    pub device_id: String,
+    pub application_ids: ApplictaionID
+}
+
+#[derive(Debug, serde::Serialize, serde::Deserialize)]
+pub struct EndDevices {
+    pub end_devices: Vec<Device>
+}
+
+#[derive(Debug, serde::Serialize, serde::Deserialize)]
+pub struct Device {
+    pub ids: DeviceId
+}
+
--- lorawan-counter/src/main.rs
+++ lorawan-counter/src/main.rs
@@ -1,9 +1,12 @@
 use std::rc::Rc;
+use futures_util::StreamExt;
 use reqwest::header;
 use slint::{VecModel, SharedString, ModelRc};
 
 pub(crate) mod entity;
 const APPLITIONS: &'static str = "https://lora.heltec.org/api/v3/applications";
+const DEVICES: &'static str = "https://lora.heltec.org/api/v3/applications/{}/devices";
+const EVENTS: &'static str = "https://lora.heltec.org/api/v3/events";
 
 slint::include_modules!();
 
@@ -11,9 +14,7 @@ slint::include_modules!();
 async fn main() {
     let app = MainWindow::new().unwrap();
     let weak = app.as_weak();
-    slint::invoke_from_event_loop(||{
-        println!("123");
-    });
+
     app.global::<State>().on_enter_key(move |v| {
         let app1 = weak.clone();
         let app = weak.unwrap();
@@ -22,7 +23,7 @@ async fn main() {
         let token = v.trim(); 
         headers.insert(header::AUTHORIZATION, header::HeaderValue::from_str(&(String::from("Bearer ") + token)).unwrap());
         let builder = reqwest::ClientBuilder::new().default_headers(headers);
-        tokio::spawn(async move {
+        slint::spawn_local(async move {
             let a = app1.clone();
             let s: entity::ApplictaionResp = builder.build().unwrap().get(APPLITIONS).send().await.unwrap().json().await.unwrap();
             slint::invoke_from_event_loop(move || {
@@ -35,8 +36,29 @@ async fn main() {
                 let the_model : Rc<VecModel<SharedString>> = Rc::new(VecModel::from(v));
                 let the_model_rc = ModelRc::from(the_model.clone());
                 app.set_model(the_model_rc);
-            });
-        });
+            }).unwrap();
+        }).unwrap();
+    });
+
+    app.global::<State>().on_listen(move |token, application| {
+        let mut headers = header::HeaderMap::new();
+        let token = token.trim();
+        headers.insert(header::AUTHORIZATION, header::HeaderValue::from_str(&(String::from("Bearer ") + token)).unwrap());
+        let builder = reqwest::ClientBuilder::new().default_headers(headers);
+        slint::spawn_local(async move {
+            let client = builder.build().unwrap();
+            let url = DEVICES.replace("{}", &application);
+            let devices: entity::EndDevices  = client.get(url).send().await.unwrap().json().await.unwrap();
+            println!("{:?}", devices);
+            let devices: Vec<_> = devices.end_devices.into_iter().map(|d| entity::Identifier {device_ids: d.ids}).collect();
+            let idents = entity::Identifiers { identifiers: devices, names: vec![String::from("as.up.data.forward")] };
+            let mut stream = client.post(EVENTS).json(&idents).send().await.unwrap()
+                .bytes_stream();
+
+            while let Some(item) = stream.next().await {
+                println!("Chunk: {:?}", item);
+            }
+        }).unwrap();
     });
     app.run().unwrap();
 }

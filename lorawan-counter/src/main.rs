28eff30b1fa52cf34e20f97d5b75ea8d04642203
vxshug <vxzyfx@gmail.com>
2023-09-21 11:52:24 +0800

    feat: add

--- lorawan-counter/src/main.rs
+++ lorawan-counter/src/main.rs
@@ -1,7 +1,9 @@
+use std::{cell, fs};
 use std::rc::Rc;
-use futures_util::StreamExt;
+use futures_util::{FutureExt, StreamExt};
 use reqwest::header;
-use slint::{VecModel, SharedString, ModelRc};
+use slint::{VecModel, SharedString, ModelRc, Model};
+use crate::entity::{ApplictaionID, DeviceId, Identifier};
 
 pub(crate) mod entity;
 const APPLITIONS: &'static str = "https://lora.heltec.org/api/v3/applications";
@@ -13,11 +15,14 @@ slint::include_modules!();
 #[tokio::main]
 async fn main() {
     let app = MainWindow::new().unwrap();
-    let weak = app.as_weak();
+    let weak1 = app.as_weak();
+    let weak2 = app.as_weak();
+
+    let mut handler = None::<slint::JoinHandle<()>>;
 
     app.global::<State>().on_enter_key(move |v| {
-        let app1 = weak.clone();
-        let app = weak.unwrap();
+        let app1 = weak1.clone();
+        let app = weak1.unwrap();
         let model = app.get_model();
         let mut headers = header::HeaderMap::new();
         let token = v.trim(); 
@@ -41,24 +46,69 @@ async fn main() {
     });
 
     app.global::<State>().on_listen(move |token, application| {
+        let app = weak2.clone();
+        let app = app.unwrap();
         let mut headers = header::HeaderMap::new();
         let token = token.trim();
         headers.insert(header::AUTHORIZATION, header::HeaderValue::from_str(&(String::from("Bearer ") + token)).unwrap());
         let builder = reqwest::ClientBuilder::new().default_headers(headers);
-        slint::spawn_local(async move {
+        let h = slint::spawn_local(async move {
             let client = builder.build().unwrap();
             let url = DEVICES.replace("{}", &application);
-            let devices: entity::EndDevices  = client.get(url).send().await.unwrap().json().await.unwrap();
-            println!("{:?}", devices);
-            let devices: Vec<_> = devices.end_devices.into_iter().map(|d| entity::Identifier {device_ids: d.ids}).collect();
-            let idents = entity::Identifiers { identifiers: devices, names: vec![String::from("as.up.data.forward")] };
-            let mut stream = client.post(EVENTS).json(&idents).send().await.unwrap()
-                .bytes_stream();
+            let old_devices: Vec<_> = app.get_devices().iter().collect();
 
-            while let Some(item) = stream.next().await {
-                println!("Chunk: {:?}", item);
+            let devices: entity::EndDevices  = client.get(url).send().await.unwrap().json().await.unwrap();
+            let mut devices: Vec<_> = devices.end_devices.into_iter().map(|d| entity::Identifier {device_ids: d.ids}).collect();
+            let list: Vec<_> = devices.iter().map(|e| DeviceItem {name: SharedString::from(&e.device_ids.device_id), count: 0}).collect();
+
+            let list: Vec<_> = list.into_iter().map(| mut dev| {
+                for old_device in &old_devices {
+                    if old_device.name == dev.name {
+                        dev.count = old_device.count;
+                    }
+                };
+                dev
+            }).collect();
+
+            let list = Rc::new(slint::VecModel::from(list));
+            app.set_devices(list.into());
+            let idents = entity::Identifiers { identifiers: devices, names: vec![String::from("as.up.data.forward")] };
+            loop {
+                let mut stream = client.post(EVENTS).json(&idents).send().await.unwrap()
+                    .bytes_stream();
+                while let Some(item) = stream.next().await {
+                    let mut devices: Vec<_> = app.get_devices().iter().collect();
+                    if let Ok(byte) = item {
+                        let res = String::from_utf8_lossy(byte.as_ref());
+                        if res.find("events.stream.start").is_some() {
+                            continue;
+                        }
+                        println!("Chunk: {:?}", res);
+                        if res.find("error").is_some() {
+                            fs::write("./log.txt", res.as_bytes()).unwrap();
+                            break;
+                        }
+                        let v: Vec<_> = devices.into_iter().map(|mut device|  {
+                            let s = format!("\"device_id\":\"{}\"", device.name);
+                            match res.find(&s) {
+                                None => {device}
+                                Some(_) => {
+                                    device.count += 1;
+                                    device
+                                }
+                            }
+                        }).collect();
+                        let list = Rc::new(slint::VecModel::from(v));
+                        app.set_devices(list.into());
+                    }
+                }
+                println!("CLOSE");
             }
         }).unwrap();
+        let old = handler.take();
+        old.map(|o| o.abort());
+        handler.replace(h);
+
     });
     app.run().unwrap();
 }
